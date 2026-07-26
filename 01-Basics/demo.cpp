5e15348c94df25fe89203bfa763747878a2e558c
Krish Agrawal <3616krishagrawal@gmail.com>
2026-07-26 11:30:56 +0530

    Add cpp  program

---
+++ 01-Basics/demo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+
+    cout << "Enter your name: ";
+    cin >> name; // Taking input from the user
+
+    cout << "Hello, " << name << "! Welcome to C++." << endl;
+
+    return 0;
+}
