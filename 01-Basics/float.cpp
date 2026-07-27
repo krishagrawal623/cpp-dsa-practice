aa105269655ec4ebbdae5062a5e93ec0be540087
Krish Agrawal <3616krishagrawal@gmail.com>
2026-07-27 20:05:21 +0530

    Add flaot datatype  program

---
+++ 01-Basics/float.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float x = 9;
+    float y = 6 ;
+    cout<<"value of x :"<<x<<endl;
+    cout<<"value of y :"<<y<<endl;
+    cout<<"addition of x and y is :"<<x+y<<endl;
+    cout<<"substraction of x and y is :"<<x-y<<endl;
+    cout<<"multiplication of x and y is :"<<x*y<<endl;
+    cout<<"division of x and y is :"<<x/y<<endl;
+}
