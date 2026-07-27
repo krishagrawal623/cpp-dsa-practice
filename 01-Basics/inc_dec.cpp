432b8ed50bf0469be2f48336c3b6aeb21f2a7357
Krish Agrawal <3616krishagrawal@gmail.com>
2026-07-27 20:06:06 +0530

    Add increment/decrement  program

---
+++ 01-Basics/inc_dec.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 6 ;
+    cout<<x<<endl;
+    x++;
+    cout<<x<<endl;
+    x--;
+    cout<<x<<endl;
+    ++x;
+    cout<<x<<endl;
+    --x;
+    cout<<x<<endl;
+}
