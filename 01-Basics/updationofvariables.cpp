8fa33fddaea5d55d2533af85d5d42f727c8ed69b
Krish Agrawal <3616krishagrawal@gmail.com>
2026-07-29 12:23:34 +0530

    .

---
+++ 01-Basics/updationofvariables.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 60;
+    cout<<x<<endl;
+
+    x = 4;
+    cout<<x<<endl;
+
+    // x = x + 3;
+    x += 3;
+    cout<<x<<endl;
+}
+    
