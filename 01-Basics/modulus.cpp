f430a3de7986535166dff7dd21e6b05ad613d7ac
Krish Agrawal <3616krishagrawal@gmail.com>
2026-07-29 12:19:57 +0530

    .

---
+++ 01-Basics/modulus.cpp
@@ -0,0 +1,19 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     float x , y;
+//     cout<<"value of x is  ";
+//     cin>>x;
+//     cout<<"value of y is  ";
+//     cin>>y;
+//     cout<<"modulus of two no. is :"<<fmod(x ,y)<<endl;
+
+// }
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a = -9;
+    int b = 5;
+    cout<<a%b;
+}
