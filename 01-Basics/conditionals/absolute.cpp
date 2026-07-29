860e54efcd1a23f6311f73f7fcab0f3eb725db9e
Krish Agrawal <3616krishagrawal@gmail.com>
2026-07-29 12:25:33 +0530

    .

---
+++ 01-Basics/conditionals/absolute.cpp
@@ -0,0 +1,42 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x;
+//     cout<<"Enter the number :";
+//     cin >> x;
+//     if (x>0){
+//         cout<<"No.is positive";
+
+//     }
+//     else{
+//         cout<<abs(x);
+//     }
+// }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x;
+//     cout<<"Enter the number :";
+//     cin >> x;
+//     if (x>0){
+//         cout<<"No.is positive";
+
+//     }
+//     else{
+//         cout<<-x;
+//     }
+// }
+// if we want to change to its absolute value
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number :";
+    cin >> n;
+    if (n<0){
+    n = -n;
+
+        cout<<n;
+    }
+
+}
