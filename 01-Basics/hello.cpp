be9833b91dffe35e8cf292ad9385ea830a53688a
Krish Agrawal <3616krishagrawal@gmail.com>
2026-07-29 12:22:18 +0530

    .

---
+++ 01-Basics/hello.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+     
+cout<<"hello"<<"\n"<<"world!";
+//     cout<<"hello world!";
+//     cout<<endl;//to give space in between lines
+//     cout<<"hello world!"; 
+
+
+}
