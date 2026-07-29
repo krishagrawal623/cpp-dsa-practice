16c26057205c29ae4c61aebc87dde173aec4f209
Krish Agrawal <3616krishagrawal@gmail.com>
2026-07-29 12:20:59 +0530

    .

---
+++ 01-Basics/sum.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x , y, z;
+    cout<<"value of x is  ";
+    cin>>x;
+    cout<<"value of y is  ";
+    cin>>y;
+    cout<<"sum of two no. is :"<<x + y<<endl;
+
+}
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x , y, z;
+//     cout<<"value of x is  ";
+//     cin>>x;
+//     cout<<"value of y is  ";
+//     cin>>y;
+//     cout<<"value of z is  ";
+//     cin>>z;
+//     cout<<"product of three no. is :"<<x*y*z<<endl;
+
+// }
