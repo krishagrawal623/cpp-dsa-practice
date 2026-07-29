fc5e50c3ab6d5f9f82bd61aeddabaf8abaf8edc2
Krish Agrawal <3616krishagrawal@gmail.com>
2026-07-29 12:20:29 +0530

    .

---
+++ 01-Basics/simpleinterest.cpp
@@ -0,0 +1,10 @@
+// calculation of simple interest
+#include<iostream>
+using namespace std;
+int main(){
+    float p = 3450;
+    float r = 3.5;
+    float t = 5;
+    float si = (p*r*t)/100;
+    cout<<si<<endl;
+}
