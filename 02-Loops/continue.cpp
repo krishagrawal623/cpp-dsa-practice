7b0625a54bcc0d038c59cf677b338271ea2d0e1c
Krish Agrawal <3616krishagrawal@gmail.com>
2026-08-02 12:02:50 +0530

    initial commit

---
+++ 02-Loops/continue.cpp
@@ -0,0 +1,20 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(int i =1;i<=20;i++){
+//     if(i==3 || i==7) continue;
+//         cout<<i<<" ";
+//     }
+    
+    
+
+// }
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=1;i<=100;i++){
+    if(i%2==0) continue;
+        cout<<i<<" ";
+    }
+    
+    }
