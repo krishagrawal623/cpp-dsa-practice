f87804d441096ecdd4a66b33e02aaf2a91ffadff
Krish Agrawal <3616krishagrawal@gmail.com>
2026-08-02 12:01:56 +0530

    initial commit

---
+++ 02-Loops/asciivalues.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 65;
+    while(n<=97){
+    // cout<<n<<endl;
+    // n++;
+    char ch = (char)n;
+    cout<<n<<" "<<ch<<endl;
+    n++;
+}
+}
+// }
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+//     char ch = 'a';
+//     while(ch<='z'){
+//     // cout<<n<<endl;
+//     int i = (int)ch;
+//     cout<<ch<<" "<<i<<endl;
+//      ch++;
+//     // int i = (int)ch;
+//     // cout<<ch<<" "<<i<<endl;
+// }
+    
+// } 
