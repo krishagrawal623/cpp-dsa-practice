13f8e9bcdad4b294ba7b3ca291f8ca0e86f0585b
Krish Agrawal <3616krishagrawal@gmail.com>
2026-08-03 19:10:28 +0530

    initial commit

---
+++ 02-Loops/factorial.cpp
@@ -0,0 +1,30 @@
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+//   int n;
+//   cout<<"enter a number :";
+//   cin>>n;
+//   int product = 1;
+//   for(int i=1;i<=n;i++) {
+//         product*=i;
+                
+//     }
+//     cout << product; 
+// }
+#include<iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cout<<"enter a number :";
+  cin>>n;
+  long long  product = 1;
+  for(int i=1;i<=n;i++) {
+        product*=i;
+        cout << i<<"!="<<product;
+        
+                
+    }
+    
+}
