1aa921348afc9fcb9b8a475948e2968c27f69215
Krish Agrawal <3616krishagrawal@gmail.com>
2026-08-04 17:23:55 +0530

    initial commit

---
+++ 02-Loops/gp.cpp
@@ -0,0 +1,33 @@
+// 1 2 4 8 16 32
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     int a = 1;
+//     for (int i=1;i<=n;i++){
+//         cout<<a<<" ";
+//         a = a*2;
+//     }}
+// 5 15 45 135
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     int a = 5;
+//     for (int i=1;i<=n;i++){
+//         cout<<a<<" ";
+//         a = a*3;
+//     }}
+// 3,12,48....n
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a = 3;
+    for (int i=1;i<=n;i++){
+        cout<<a<<" ";
+        a = a*4;
+    }}
