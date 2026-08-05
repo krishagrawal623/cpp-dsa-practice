d8ebb4e8119256635b3930080a2f6b12f22f2192
Krish Agrawal <3616krishagrawal@gmail.com>
2026-08-05 17:07:41 +0530

    initial commit

---
+++ 02-Loops/tableof19.cpp
@@ -0,0 +1,24 @@
+// table of 19
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     // int n;
+//     // cin>>n;
+//     for(int i=19;i<=190;i=i+19){
+//         // if(i%19==0)
+//         cout<<i<<" ";
+//     }
+// }
+
+// table of any number
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=n;i<=n*10;i=i+n){
+       
+        cout<<i<<" ";
+    }
+}
+
