c697c24d93c53d542677a3b7c8802bf110112a8b
Krish Agrawal <3616krishagrawal@gmail.com>
2026-08-05 17:03:55 +0530

    initial commit

---
+++ 02-Loops/printno.cpp
@@ -0,0 +1,57 @@
+// printing even no. using if cond.
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        if(i%2==0)
+        cout<<i<<"\n";
+    }
+}
+// printing odd no. using if cond.
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=1;i<=n;i++){
+//         if(i%2!=0)
+//         cout<<i<<"\n";
+        
+//     }
+// }
+// printing symbols
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=1;i<=n;i++){
+//         if(i%2!=0)
+//         cout<<"*"<<"\n";
+//         else cout<<","<<endl;
+//     }
+// }
+// printing even no. without ifelse
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=2;i<=n;i=i+2){
+//         // if(i%2==0)
+//         cout<<i<<" ";
+//     }
+// }
+// printing odd no. without ifelse
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=1;i<=n;i=i+2){
+//         // if(i%2==0)
+//         cout<<i<<" ";
+//     }
+// }
