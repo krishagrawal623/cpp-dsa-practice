b341c1f4145f91783e7cbefdb18400c241aed9c0
Krish Agrawal <3616krishagrawal@gmail.com>
2026-08-05 17:04:47 +0530

    initial commit

---
+++ 02-Loops/reverse.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cout<<"enter a number :";
+  cin>>n;
+  int sum = 0;
+  while(n!=0){
+    int ld = n%10;
+    sum = sum*10;
+    sum = sum+ld;
+    n = n/10;
+  }
+  cout<<sum;
+}
+    
