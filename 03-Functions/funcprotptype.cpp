3e8e214e6250767a430f392865529ebf93d723d2
Krish Agrawal <3616krishagrawal@gmail.com>
2026-08-06 16:17:39 +0530

    initial commit

---
+++ 03-Functions/funcprotptype.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int add(int  ,int );
+//     int sum = num1+num2;
+//     return sum;
+// }
+int main(){
+    int a = 5;
+    int b = 4;
+    cout<<add(a,b)<<endl;
+     
+    return 0;
+}
+int add(int num1 ,int num2){
+    int sum = num1+num2;
+    return sum;
+}
