de42ef41ebcc000d077a000d32c0e782adfec47b
Krish Agrawal <3616krishagrawal@gmail.com>
2026-08-06 16:19:00 +0530

    initial commit

---
+++ 03-Functions/stt.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    // cout<<pow(2,3.1)<<endl;
+    // cout<<sqrt(17)<<endl
+    double ans=sqrt(24);
+    cout<<ans<<endl;
+
+    return 0;
+}
