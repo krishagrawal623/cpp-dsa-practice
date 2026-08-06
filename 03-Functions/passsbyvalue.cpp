35bc870a9b26deb9830f0eb38cc7bd1ca5c7cd50
Krish Agrawal <3616krishagrawal@gmail.com>
2026-08-06 16:18:42 +0530

    initial commit

---
+++ 03-Functions/passsbyvalue.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+void changevalue(int z){
+    z = 100;
+
+}
+
+int main(){
+    int a = 5;
+    
+    changevalue(a);
+    cout<<a<<endl;
+   
+     
+    return 0;
+}
