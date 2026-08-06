989f83e40dc01d105f8f9e37b65d5a9616dfdc71
Krish Agrawal <3616krishagrawal@gmail.com>
2026-08-06 16:18:07 +0530

    initial commit

---
+++ 03-Functions/passbyref.cpp
@@ -0,0 +1,35 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int p = 5;
+//     int&q = p;
+//     q++;
+//     cout<<p<<endl;
+
+//     cout<<&p<<endl;
+//     cout<<&q<<endl;
+
+
+   
+     
+//     return 0;
+// }
+
+#include<iostream>
+using namespace std;
+
+void changevalue(int &z){
+    z = 100;
+
+}
+
+int main(){
+    int a = 5;
+    
+    changevalue(a);
+    cout<<a<<endl;
+     
+     
+    return 0;
+}
