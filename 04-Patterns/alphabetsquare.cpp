22e9bc6794747da4f7c63f5155145551149c1956
Krish Agrawal <3616krishagrawal@gmail.com>
2026-08-09 12:38:29 +0530

    initial commit

---
+++ 04-Patterns/alphabetsquare.cpp
@@ -0,0 +1,75 @@
+// ABCD
+// ABCD
+// ABCD
+// ABCD
+// #include<iostream>
+// using namespace std;
+// int main(){
+//   int m;
+//   cout<<"Enter m :";
+//   cin>>m;
+//   for(int i = 1;i<=m;i++){
+//     for(char j = 'A';j<='D';j++){
+//       cout<<j;
+//     }
+//      cout<<endl;
+//   }
+  
+
+//  }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//   int m;
+//   cout<<"Enter m :";
+//   cin>>m;
+//   for(int i = 1;i<=m;i++){
+//     for(int  j = 1;j<=4;j++){
+//       cout<<(char)(j+64);
+//     }
+//      cout<<endl;
+//   }
+  
+
+// }
+
+// AAAA
+// BBBB
+// CCCC
+// DDDD
+#include<iostream>
+using namespace std;
+int main(){
+  int m;
+  cout<<"Enter m :";
+  cin>>m;
+  for(int i = 1;i<=m;i++){
+    for(int  j = 1;j<=4;j++){
+      cout<<(char)(i+64);
+    }
+     cout<<endl;
+  }
+  
+
+}
+
+// abc
+// abc
+// abc
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//   int m;
+//   cout<<"Enter m :";
+//   cin>>m;
+//   for(int i = 1;i<=m;i++){
+//     for(int  j = 1;j<=m;j++){
+//       cout<<(char)(j+96);
+//     }
+//      cout<<endl;
+//   }
+  
+
+// }
