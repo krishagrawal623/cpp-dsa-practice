79d394914c39a168ea395a65fc8beb37938d6a11
Krish Agrawal <3616krishagrawal@gmail.com>
2026-08-09 12:39:48 +0530

    initial commit

---
+++ 04-Patterns/starrectangle.cpp
@@ -0,0 +1,18 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//   int m;
+//   cout<<"Enter m :";
+//   cin>>m;
+//   int n;
+//   cout<<"Enter n :";
+//   cin>>n;
+//   for(int i = 1;i<=m;i++){
+//     for(int j = 1;j<=n;j++){
+//       cout<<"* ";
+//     }
+//      cout<<endl;
+//   }
+  
+
+// }
