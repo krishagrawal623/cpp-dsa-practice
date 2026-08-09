a44056617dec3a54e03d20eefb1a19f0946373bb
Krish Agrawal <3616krishagrawal@gmail.com>
2026-08-09 12:41:24 +0530

    initial commit

---
+++ 05-Arrays/0_Array.cpp
@@ -0,0 +1,111 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int array[] = {1,2,3,4,5};
+
+//     for(int i = 0; i < 5;i++){
+//         cout<<array[i]<<endl;
+        
+//     }
+//     cout<<sizeof(array)<<endl;
+//     cout<<sizeof(array)/sizeof(array[0])<<endl;
+//     return 0;
+// }
+
+//  #include<iostream>
+// using namespace std;
+// int main(){
+//     int array[5];
+
+//     for(int i = 0; i < 5;i++){
+//         cout<<array[i]<<endl;
+        
+//     }
+//     cout<<sizeof(array)<<endl;
+//     cout<<sizeof(array)/sizeof(array[0])<<endl;
+//     return 0;
+// }
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int array[] = {1,2,3,4,5};
+//     //for each loop
+//     for(int ele:array){
+//         cout<<ele<<endl;
+//     }
+
+        
+//     }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int array[] = {1,2,3,4,5};
+//     int size = sizeof(array)/sizeof(array[0]);
+//     int index = 0;
+//     //while loop
+//     while(index<size){
+//         cout<<array[index]<<endl;
+//         index++;
+//     }
+    
+    
+//     }
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     char vowels[5];
+
+//     for(int idx=0; idx<5; idx++){
+//     cin>>vowels[idx];
+//     }
+//     for(int idx=0; idx<5; idx++){
+//     cout<<vowels[idx]<<" ";
+//     }
+
+//     return 0;
+
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     char vowels[5];
+//     //for each loop 
+
+//     for(char &element:vowels){
+//     cin>>vowels[idx];
+//     idx++
+//     }
+//     for(int idx=0; idx<5; idx++){
+//     cout<<vowels[idx]<<" ";
+//     }
+
+//     return 0;
+
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     char vowels[5];
+//     int idx = 0;
+//     //while loop 
+
+//     while(idx<5){
+//     cin>>vowels[idx];
+//     idx++;
+//     }
+//     for(int idx=0; idx<5; idx++){
+//     cout<<vowels[idx]<<" ";
+//     }
+
+//     return 0;
+// }
+
