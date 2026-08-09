5468e231c6a20660672a34ea04fb6939aa02b486
Krish Agrawal <3616krishagrawal@gmail.com>
2026-08-09 12:38:56 +0530

    initial commit

---
+++ 04-Patterns/demoques.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i = 1;i<=10;i++){
+        for(int j = i;j<=i;j++){
+            
+        }
+      cout<<'*' << "  "<<'*'<<endl;  
+    }
+    
+
+}
