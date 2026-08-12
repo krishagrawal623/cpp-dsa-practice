9ef1b22e5c3c0c1e1086e4363c37b64ba77e18df
Krish Agrawal <3616krishagrawal@gmail.com>
2026-08-12 16:27:42 +0530

    Add query element problem

---
+++ 05-Arrays/09_queryelement.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+
+    vector <int> v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+
+    const int N = 1e5 + 10;
+    vector <int> freq(N , 0);
+    for(int i=0;i<n;i++){
+        freq[v[i]]++;
+}
+
+cout<<"enter quries: ";
+int q;
+cin>>q;
+while(q--){
+    int queryelement;
+    cin>>queryelement;
+    cout<<freq[queryelement];
+}
+}
