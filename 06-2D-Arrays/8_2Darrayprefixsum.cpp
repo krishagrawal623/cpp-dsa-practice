fadfae075ae4ed3c8bb565c976713bd74501bcc4
Krish Agrawal <3616krishagrawal@gmail.com>
2026-08-16 11:20:26 +0530

    prefix sum problem

---
+++ 06-2D-Arrays/8_2Darrayprefixsum.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int rectanglesum(vector<vector<int> > &matrix, int l1, int r1, int l2, int r2){
+
+    int sum = 0;
+    for(int i = l1;i<=l2;i++){
+        for(int j = r1;j<= r2;j++){
+            sum+= matrix[i][j];
+        }
+    }
+
+    return sum;
+}
+
+
+int main(){
+
+    int m,n;
+    cin>>n>>m;
+
+    vector<vector<int> > matrix(n,vector<int> (m));
+
+    for(int i = 0;i<n;i++){
+        for(int j = 0;j<m;j++){
+            cin>>matrix[i][j];
+        }
+    }
+
+    int l1,r1,l2,r2;
+    cin>>l1>>r1>>l2>>r2;
+
+    for(int i = 0;i<n;i++){
+        for(int j = 0;j<m;j++){
+            cout<<matrix[i][j]<<" ";
+        }cout<<endl;
+    }
+
+    int sum = rectanglesum(matrix,l1,r1,l2,r2);
+    cout<<sum;
+
+
+}
