de799584ebae625240c5103d5e504e4b592f9566
Krish Agrawal <3616krishagrawal@gmail.com>
2026-09-16 19:21:03 +0530

    initial commit

---
+++ 08-practice/primerange.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    vector<bool> isPrime(n + 1, true);
+
+    if (n >= 0) isPrime[0] = false;
+    if (n >= 1) isPrime[1] = false;
+
+    for (int i = 2; i * i <= n; i++) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    cout << "Prime numbers up to " << n << " are:\n";
+
+    for (int i = 2; i <= n; i++) {
+        if (isPrime[i]) {
+            cout << i << " ";
+        }
+    }
+
+    return 0;
+}
+
+
+
+
+
