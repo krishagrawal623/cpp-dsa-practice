e055a3bda2104c4122cb6ff7a5fb474eb3dbd55b
Krish Agrawal <3616krishagrawal@gmail.com>
2026-09-18 11:35:52 +0530

    initial commit

---
+++ 09-Revision/Basics.cpp
@@ -0,0 +1,24 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int x, y;
+
+//     cout << "Enter value of x: ";
+//     cin >> x;
+
+//     cout << "Enter value of y: ";
+//     cin >> y;
+
+//     cout << x + y;
+
+//     return 0;
+// }
+
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int x, y;
